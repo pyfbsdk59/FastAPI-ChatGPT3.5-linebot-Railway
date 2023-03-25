e3d235c1708a4c4901422238fadf8cddf884f9d6
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-03-25 19:27:22 +0800

    3c

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import logging
+import logging, uvicorn
 from fastapi import FastAPI, Request, HTTPException
 
 from linebot import LineBotApi, WebhookHandler
@@ -94,39 +94,5 @@ def handling_message(event):
 
 
 
-'''
-@app.post("/callback") # 指定 api 路徑 (get方法)
-def callback(request):
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-'''
-'''
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # Get user's message
-    user_message = event.message.text
-
-
-    reply_msg = chatgpt.get_response(user_message)
-    
-    
-    print(reply_msg)
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_msg)
-    )
-    '''
-
-
-
-
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
